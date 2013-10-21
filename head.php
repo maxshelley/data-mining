084b61562c9df7d4651d943aea80b283aeabd5e3
Max Shelley <max@measuredbrilliance.com>
2013-10-21 17:28:47 +0100

    Deleted some unused files. Formatting, replacing some js.

--- head.php
+++ head.php
@@ -7,7 +7,8 @@
     <title>Atare</title>
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     <link rel="stylesheet" href="css/atare.css">
-    <script src="js/jquery-1.8.3.min.js"></script>
+    <!--<script src="js/jquery-1.8.3.min.js"></script>-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="js/joverlay.min.js"></script>
     <script src="js/scripts-ck.js"></script> 
     
--- js/scripts-ck.js
+++ js/scripts-ck.js
@@ -214,3 +214,27 @@ document.createElement("section");
 // you can just add these into the head as normal .js files and they'll work.
 
 //@codekit-prepend "parallax.js", "animateNumbers.js","vendor/bootstrap-affix.js"
+
+// Replacing the original hack day javascript with something (hopefully) less wordy.
+
+function calculateTotals() {
+
+  var year1999 = 724400000000;
+
+  console.log(year1999);
+
+}
+
+
+$(document).ready(function(){
+
+  $( ".timeline-circles" ).click(function() { // Click on a bubble on the chart.
+
+    $('.generated-infographic').show(); // Show the infographic container.
+
+    calculateTotals();
+
+  });
+
+});
+
